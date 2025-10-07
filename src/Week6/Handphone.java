a5d81f0bcf0d797e52b04886794d8d8d5e342a45
Malv jahanto <MalvinB30>
2025-10-07 20:19:48 +0700

    Order

--- src/Week6/Handphone.java
+++ src/Week6/Handphone.java
@@ -1,5 +1,14 @@
 package Week6;
 
-public class Handphone {
+public class Handphone extends Barang {
+    private String warna;
+    public static int total = 0;
 
+    public Handphone(int id, String nama, double harga, int stok, String warna) {
+        super(id, nama, harga, stok);
+        this.warna = warna;
+    }
+
+    public String getWarna() { return warna; }
 }
+
--- src/Week6/Voucher.java
+++ src/Week6/Voucher.java
@@ -1,5 +1,16 @@
 package Week6;
 
-public class Voucher {
+public class Voucher extends Barang {
+    private double pajak;
+    public static int total = 0;
 
+    public Voucher(int id, String nama, double harga, int stok, double pajak) {
+        super(id, nama, harga, stok);
+        this.pajak = pajak;
+    }
+
+    public double getHargaJual() {
+        return harga + (harga * pajak);
+    }
 }
+
